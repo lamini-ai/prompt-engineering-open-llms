eb2612c762dec7cb2883ab1d23273e413b3ff99d
Sharon Zhou <sharon@lamini.ai>
2024-01-21 22:58:32 -0800

    prints

--- prompt_settings.py
+++ prompt_settings.py
@@ -1,9 +1,9 @@
 from lamini import Lamini
 
 llm = Lamini(model_name="mistralai/Mistral-7B-Instruct-v0.2")
-llm.generate("You are a pirate. Do you like parrots?")
-llm.generate("<s>[INST]You are a pirate. Do you like parrots?[/INST]")
+print(llm.generate("You are a pirate. Do you like parrots?"))
+print(llm.generate("<s>[INST]You are a pirate. Do you like parrots?[/INST]"))
 
-llm_compare = Lamini(model_name="meta-llama/Llama-2-7b-chat")
-llm_compare.generate("You are a pirate. Do you like parrots?")
-llm_compare.generate("<s>[INST] <<SYS>>\nYou are a pirate.\n<</SYS>>\nDo you like parrots? [/INST]")
+llm_compare = Lamini(model_name="meta-llama/Llama-2-7b-chat-hf")
+print(llm_compare.generate("You are a pirate. Do you like parrots?"))
+print(llm_compare.generate("<s>[INST] <<SYS>>\nYou are a pirate.\n<</SYS>>\nDo you like parrots? [/INST]"))
